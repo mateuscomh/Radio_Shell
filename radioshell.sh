22d52ef2d6a455ee40f066ee2e566cf54154a879
Matheus Martins <matheuscumth@gmail.com>
2021-10-16 18:47:07 -0300

    Adicionado primeiro menu

--- radioshell.sh
+++ radioshell.sh
@@ -1,2 +1,15 @@
-!# /usr/bin/env bash
+#! /usr/bin/env bash
+
+read -p "
+Radio WEB via Shell
+Escolha a categoria da rádio
+
+-1- Rock
+-2- Pop
+-3- Nacional
+-4- Aleatória
+-5- Todas
+-6- Cancelar
+
+Digite a opcao desejada" OPC
 
